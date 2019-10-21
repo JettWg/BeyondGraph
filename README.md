07bb6c6f6af53baeabbd9c1819df29007ae682e6
JettWg <the.jw.night@gmail.com>
2019-10-22 00:05:17 +0800

    add README

---
+++ README.md
@@ -0,0 +1,2 @@
+## BeyondGraph
+
--- bulk-loading/bin/bulk-loading.sh
+++ bulk-loading/bin/bulk-loading.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-java -cp target/hyperlink-tools-1.0-SNAPSHOT.jar org.hyperlink.Import "$@"
-
-#java -cp target/hyperlink-tools-1.0-SNAPSHOT.jar org.hyperlink.Import -D -c src/test/resources/import.properties --nodes=person=data/v_person.csv --relationships=data/edges.csv --edgeLabels=KNOWS,MET
+java -cp target/bulk-loading-1.0-SNAPSHOT.jar org.beyond.graph.bulk.loading.Import "$@"
 
 
-#./bin/bulk-loading.sh -D --threads=4 -c src/test/resources/import.properties \
-#--nodes=actor=data/movie/actor.csv \
-#--nodes=director=data/movie/director.csv \
-#--nodes=category=data/movie/category.csv \
-#--nodes=movie=data/movie/movie.csv \
-#--edgeLabels=BELONGS,ACT,DIRECT \
-#--relationships=data/movie/edge_belongs.csv \
-#--relationships=data/movie/edge_acted.csv \
-#--relationships=data/movie/edge_directed.csv
+#./bin/bulk-loading.sh -D \
+# --threads=4 \
+# --config=conf/import.properties \
+# --remote=conf/remote.yaml \
+# --nodes=actor=data/movie/actor.csv \
+# --nodes=director=data/movie/director.csv \
+# --nodes=category=data/movie/category.csv \
+# --nodes=movie=data/movie/movie.csv \
+# --edgeLabels=BELONGS,ACT,DIRECT \
+# --relationships=data/movie/edge_belongs.csv \
+# --relationships=data/movie/edge_acted.csv \
+# --relationships=data/movie/edge_directed.csv
 
